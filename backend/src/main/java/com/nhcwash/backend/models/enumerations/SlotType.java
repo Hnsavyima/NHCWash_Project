10ec9a21b790ffb5ae120e13bfb6e51d77f51fc2
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-01-25 15:17:14 +0100

    pickup fonction

--- backend/src/main/java/com/nhcwash/backend/models/enumerations/SlotType.java
+++ backend/src/main/java/com/nhcwash/backend/models/enumerations/SlotType.java
@@ -4,4 +4,5 @@ public enum SlotType {
     COLLECTION, // L'employé vient chercher le linge
     DELIVERY, // L'employé ramène le linge
     DROP_OFF // Le client dépose lui-même à l'atelier
+    // il faudra peut-être ajouter un type "PICK_UP" si on veut différencier le "COLLECTION" où l'employé vient chercher le linge à domicile et le "DROP_OFF" où le client dépose lui-même le linge à l'atelier
 }
