095bbc61bb4498e10a69afca80f76e8f2861b797
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-02-02 21:29:12 +0100

    feat(enums): SlotType - PICKUP, DELIVERY

--- backend/src/main/java/com/nhcwash/backend/models/enumerations/SlotType.java
+++ backend/src/main/java/com/nhcwash/backend/models/enumerations/SlotType.java
@@ -1,7 +1,6 @@
 package com.nhcwash.backend.models.enumerations;
 
 public enum SlotType {
-    COLLECTION, // L'employé vient chercher le linge
-    DELIVERY, // L'employé ramène le linge
-    DROP_OFF // Le client dépose lui-même à l'atelier
+    PICKUP,   // Ramassage
+    DELIVERY  // Livraison
 }
