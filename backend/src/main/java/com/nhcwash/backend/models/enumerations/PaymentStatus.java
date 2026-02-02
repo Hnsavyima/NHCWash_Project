6dd10f6e0e67a80511a4561200ed5fbbc32c6212
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-02-02 21:23:52 +0100

    feat(enums): PaymentStatus - PENDING, SUCCEEDED, FAILED, REFUNDED

--- backend/src/main/java/com/nhcwash/backend/models/enumerations/PaymentStatus.java
+++ backend/src/main/java/com/nhcwash/backend/models/enumerations/PaymentStatus.java
@@ -1,7 +1,8 @@
 package com.nhcwash.backend.models.enumerations;
 
 public enum PaymentStatus {
-    UNPAID, // En attente de règlement
-    PAID, // Règlement effectué
-    REFUNDED // Remboursé en cas de litige
+    PENDING,   // En attente
+    SUCCEEDED, // Réussi
+    FAILED,    // Échoué
+    REFUNDED   // Remboursé
 }
