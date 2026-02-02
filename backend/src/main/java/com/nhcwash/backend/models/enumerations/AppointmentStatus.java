f8e5f10227b7b624097348e4e06b3104f2f6ab8c
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-02-02 21:30:44 +0100

    feat(enums): AppointmentStatus - BOOKED, CONFIRMED, RESCHEDULED, CANCELLED

---
+++ backend/src/main/java/com/nhcwash/backend/models/enumerations/AppointmentStatus.java
@@ -0,0 +1,8 @@
+package com.nhcwash.backend.models.enumerations;
+
+public enum AppointmentStatus {
+    BOOKED,       // Réservé
+    CONFIRMED,    // Confirmé
+    RESCHEDULED,  // Reporté
+    CANCELLED     // Annulé
+}
