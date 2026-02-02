08abf0025b44c6d25bace3757ab3bf483d592176
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-02-02 21:33:52 +0100

    feat(repositories): InvoiceRepository

---
+++ backend/src/main/java/com/nhcwash/backend/repositories/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package com.nhcwash.backend.repositories;
+
+import com.nhcwash.backend.models.entities.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    Optional<Invoice> findByOrder_OrderId(Long orderId);
+}
