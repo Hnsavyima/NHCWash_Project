cfcb5d42674733d3801c1fe5ca2eac7ae07546f9
NSAVYIMANA Hervé Cédric <hnsavyimana@gmail.com>
2026-05-06 21:13:36 +0200

    feat(api-docs): add OpenAPI configuration

---
+++ backend/src/main/java/com/nhcwash/backend/configs/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.nhcwash.backend.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "NHCWash API", version = "1.0", description = "Documentation de l'API NHCWash"),
+        security = { @SecurityRequirement(name = "bearerAuth") }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {}
